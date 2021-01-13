56d8e372abd99c970e50d4ad36d4fbf14913afa5
hunick1234 <59445320+hunick1234@users.noreply.github.com>
2021-01-13 23:05:21 +0800

    Update sort.go

--- sort.go
+++ sort.go
@@ -4,12 +4,18 @@ func main() {
 	fmt.Println(math)
 }
 func sort(math []int) {
-
 	for i := len(math); i > 0; i-- {
+		var count int
+		
 		for a := 0; a < i-1; a++ {
 			if math[a] < math[a+1] {
+				count++
 				math[a], math[a+1] = math[a+1], math[a]
 			}
 		}
+		if count == 0 {
+			break
+		}
+
 	}
 }
